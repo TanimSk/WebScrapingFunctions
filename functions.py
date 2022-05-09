03c81a7d155227f7d940a2c34d3cae608f073c30
Tanim Sk <sktanim5800@gmail.com>
2022-05-10 00:14:57 +0600

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,28 @@
+from selenium import webdriver 
+from selenium.webdriver.common.by import By
+
+# >> for running in headless mode
+
+# from selenium.webdriver.firefox.options import Options
+# firefox_options = Options()
+# firefox_options.set_headless()
+# driver = webdriver.Firefox(options=firefox_options)
+
+# <<
+
+driver = webdriver.Firefox()
+
+driver.get("URL")
+
+# driver.refresh()
+# driver.save_screenshot(capture_path)
+
+def send_data(xpath, keys):
+    driver.find_element(By.XPATH, xpath).clear() # clears input field, if any text is pre-written
+    driver.find_element(By.XPATH, xpath).send_keys(keys)
+
+def click_element(xpath):
+    driver.find_element(By.XPATH, xpath).click()
+
+def getText(xpath):
+    return driver.find_element(By.XPATH, xpath).text
